52ea8bb7e88a0bbebc54b33fa37535891ec58415
WalterSimper10 <solodar8975@gmail.com>
2025-10-02 11:10:17 -0400

    rename dataset

--- MyCode_project/MyCode/trainAI.py
+++ MyCode_project/MyCode/trainAI.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
 import os
-fileName = os.path.join('mushroom-dataset-1', 'train', 'images')
+import cv2
+
+import magic
+import filetype
+import imghdr
+
+
+
+fileName = os.listdir('mushroom-dataset-1')
 print(fileName)
+
+#limit tensorflow from using all the GPU memory
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
