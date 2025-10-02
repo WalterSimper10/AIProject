a4c49bbb7732176a7642f5b842aae8870a8652c5
WalterSimper10 <solodar8975@gmail.com>
2025-10-02 11:41:02 -0400

    structuring dataset

--- MyCode_project/MyCode/trainAI.py
+++ MyCode_project/MyCode/trainAI.py
@@ -2,14 +2,22 @@ import tensorflow as tf
 import os
 import cv2
 
-import magic
+#I need to use one of these to replace imghdr, but I don't know which one to use yet
 import filetype
-import imghdr
+
+data_dir = 'data' #directory to dataset
+image_exts = ['jpeg','jpg','bmp', 'png'] #literally just a list
+#^set up to loop into it
+
+#Pass through directory for 'data' folder and concatenate train + image to complete path
+os.listdir(os.path.join(data_dir,'train', 'images')) 
 
 
-
-fileName = os.listdir('mushroom-dataset-1')
-print(fileName)
+#Checking for every folder in 'data' directory
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        print(image)
+        
 
 #limit tensorflow from using all the GPU memory
 gpus = tf.config.experimental.list_physical_devices('GPU')
