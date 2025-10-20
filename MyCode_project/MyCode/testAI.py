aa3923d0006b0f3ea0ee160731eea6df71dc8638
WalterSimper10 <solodar8975@gmail.com>
2025-10-20 10:20:30 -0400

    Finally cleaning up the repo. Going to start using branches for cleaner practices

---
+++ MyCode_project/MyCode/testAI.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import keras
+from keras import layers
+from tensorflow import data as tf_data
+import matplotlib.pyplot as plt
+
+
+image_size = (180, 180)
+batch_size = 128
+
+model = keras.models.load_model(r'C:\Users\SkillsHub-Learner-09\.vscode\VSCODE Projects\AIBackend\save_at_1.keras')
+
+img = keras.utils.load_img(r"C:\Users\SkillsHub-Learner-09\.vscode\VSCODE Projects\AIBackend\kagglecatsanddogs_5340\PetImages\Cat\51.jpg", target_size=image_size)
+plt.imshow(img)
+
+img_array = keras.utils.img_to_array(img)
+img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(img_array)
+score = float(keras.ops.sigmoid(predictions[0][0]))
+print(f"This image is {100 * (1 - score):.2f}% cat and {100 * score:.2f}% dog.")
--- MyCode_project/na.py
+++
@@ -1,7 +0,0 @@
-import keras_cv
-import keras
-import tensorflow as tf
-
-print("KerasCV:", keras_cv.__version__)
-print("Keras:", keras.__version__)
-print("TensorFlow:", tf.__version__)
